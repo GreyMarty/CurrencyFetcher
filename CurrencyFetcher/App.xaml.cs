01f49cc70b260d0ce53234ca38183fd979920155
GreyMarty <ikozhan9@gmail.com>
2024-01-06 14:35:27 +0300

    Add telerik wpf project

--- CurrencyFetcher/App.xaml.cs
+++ CurrencyFetcher/App.xaml.cs
@@ -1,9 +1,28 @@
-﻿namespace CurrencyFetcher
+﻿using System.Windows;
+
+namespace CurrencyFetcher
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
+        private readonly MainWindow _mainWindow;
+
+        public App()
+        {
+            
+        }
+
+        public App(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _mainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ CurrencyFetcher/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CurrencyFetcher
+{
+    internal static class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .AddSingleton<App>()
+                .AddSingleton<MainWindow>()
+                .BuildServiceProvider();
+
+            var app = services.GetRequiredService<App>();
+            app.Run();
+        }
+    }
+}
